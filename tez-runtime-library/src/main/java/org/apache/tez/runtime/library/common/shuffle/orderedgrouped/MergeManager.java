abca39986a26ad3a008d968fb1f3b9a20500081f
Rajesh Balamohan <rbalamohan@apache.org>
2015-03-26 04:12:08 +0530

    TEZ-2214. FetcherOrderedGrouped can get stuck indefinitely when MergeManager misses memToDiskMerging (rbalamohan)
    Signed-off-by: Oleksiy Sayankin <osayankin@maprtech.com>

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/shuffle/orderedgrouped/MergeManager.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/shuffle/orderedgrouped/MergeManager.java
@@ -325,8 +325,28 @@ public class MergeManager {
 
   public void waitForInMemoryMerge() throws InterruptedException {
     inMemoryMerger.waitForMerge();
+    /**
+     * Memory released during merge process could have been used by active fetchers and if they
+     * are too fast, 'commitMemory & usedMemory' could have grown beyond allowed threshold. Since
+     * merge was already in progress, this would not have kicked off another merge and fetchers
+     * could get into indefinite wait state later. To address this, trigger another merge process
+     * if needed and wait for it to complete (to release committedMemory & usedMemory).
+     */
+    boolean triggerAdditionalMerge = false;
+    synchronized (this) {
+      if (commitMemory >= mergeThreshold) {
+        startMemToDiskMerge();
+        triggerAdditionalMerge = true;
+      }
+    }
+    if (triggerAdditionalMerge) {
+      inMemoryMerger.waitForMerge();
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Additional in-memory merge triggered");
+      }
+    }
   }
-  
+
   private boolean canShuffleToMemory(long requestedSize) {
     return (requestedSize < maxSingleShuffleLimit);
   }
