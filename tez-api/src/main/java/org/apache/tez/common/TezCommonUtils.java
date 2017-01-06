9ef001b00689d90c37b9f40e14285c5005bfbbd4
Artem Velikorodniy <avelikorodniy@maprtech.com>
2017-01-06 16:54:15 +0200

    MAPR-25723 TezUncheckedException: Duplicate Resources found with different size

--- tez-api/src/main/java/org/apache/tez/common/TezCommonUtils.java
+++ tez-api/src/main/java/org/apache/tez/common/TezCommonUtils.java
@@ -320,7 +320,7 @@ public class TezCommonUtils {
         if (originalLr != null) {
           LocalResource additionalLr = lrEntry.getValue();
           if (originalLr.getSize() != additionalLr.getSize()) {
-            throw new TezUncheckedException(
+            LOG.warn(
                 "Duplicate Resources found with different size for [" + logContext + "]: " + lrEntry.getKey() +
                     " : " + "[" + additionalLr.getResource() + "=" + additionalLr.getSize() +
                     "],[" + originalLr.getResource() + "=" + originalLr.getSize());
